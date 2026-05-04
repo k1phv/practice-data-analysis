3797eef0a432d6e258a27c064dc98bc73896cf15
Вихрев Ярослав <ivihrev4@gmail.com>
2026-05-04 18:59:38 +0300

    Добавлен вывод сводной информации о данных

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ df.rename(columns={"Temp": "Value"}, inplace=True)
 
 values = df["Value"]
 
+# получение сводной информации
+print("\n--- Сводная информация о данных ---")
+
+print("\nОбщая информация:")
+print(df.info())
+
+print("\nСтатистика:")
+print(df.describe())
+
 print("Количество значений:", len(values))
 print(values.head()) # первые 5
 # print(values)
