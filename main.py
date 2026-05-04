cd7199c6b95b92e911259a40d0d93949029403af
Вихрев Ярослав <ivihrev4@gmail.com>
2026-05-04 19:09:37 +0300

    Реализована очистка данных методом межквартильного размаха

--- main.py
+++ main.py
@@ -21,3 +21,28 @@ print(df.describe())
 print("Количество значений:", len(values))
 print(values.head()) # первые 5
 # print(values)
+
+
+# задание 2
+
+print("\n--- Очистка данных ---")
+
+before = len(df)
+
+Q1 = df["Value"].quantile(0.25)
+Q3 = df["Value"].quantile(0.75)
+IQR = Q3 - Q1
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+print("Нижняя граница:", lower_bound)
+print("Верхняя граница:", upper_bound)
+
+df = df[(df["Value"] >= lower_bound) & (df["Value"] <= upper_bound)]
+
+after = len(df)
+
+print("Количество до очистки:", before)
+print("Количество после очистки:", after)
+print("Удалено значений:", before - after)
